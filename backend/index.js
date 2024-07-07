e87e22479f29ae694f73930ca0b4e8ea990e4fd0
Mohammad Afzal <96421067+MAfzal8080@users.noreply.github.com>
2024-07-07 21:45:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 var cors = require('cors')
 require('dotenv').config()
 
-const port = process.env.port || 3818;
+const port = process.env.PORT || 3818;
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
