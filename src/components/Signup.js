cd658cd1c06910fa1f6a3979802fe92e35d1c9fd
Mohammad Afzal <96421067+MAfzal8080@users.noreply.github.com>
2024-07-07 22:05:30 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -23,7 +23,7 @@ const Signup = (props) => {
   const handleSignup = async (e)=>{
     e.preventDefault()
     const {name , email, password} = credentials;
-    const response = await fetch("http://localhost:5000/auth/createuser",{
+    const response = await fetch("https://note-z-backend.onrender.com/auth/createuser",{
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
