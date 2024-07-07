8d865c8f63185ab0adec7cf0768cabc9573533bd
Mohammad Afzal <96421067+MAfzal8080@users.noreply.github.com>
2024-07-07 21:00:24 +0530

    Delete backend directory

--- backend/dbs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const server=mongoose.connect(process.env.MONGO_URI , { retryWrites: true, w: 'majority' }).then(()=>{
-    console.log("connected")
-}).catch((err)=>{
-    console.log((err))
-});
-module.exports=server
